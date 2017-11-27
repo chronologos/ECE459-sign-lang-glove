b5d3dfdaa2f07b3047bdf12d91699d48ad512f62
Ian Tay <yi.yan.tay@duke.edu>
2017-11-27 15:53:49 -0500

    added fsm library to handle imu+gesture integration

---
+++ Projects/final_project/imu_fsm.cpp
@@ -0,0 +1,52 @@
+#include "imu_fsm.h"
+
+namespace ImuFsm {
+	state_t do_state_default(instance_data_t *data) {
+		if (data->event == GESTURE_IN) {
+			char gesture = data->key.c_str()[0];
+			switch (gesture) {
+				case 'q':
+					return STATE_WAIT_AY_Q;
+				case 'g':
+					return STATE_WAIT_AY_G;
+				case 'z':
+					return STATE_WAIT_AX;
+				case 'j':
+					return STATE_WAIT_GZ;
+				default:
+					data->event = KEY_OUT;
+					data->key = gesture;
+					return STATE_DEFAULT;
+			 }
+	  }
+		return STATE_DEFAULT;
+	}
+	
+	state_t do_state_wait(instance_data_t *data, char out, char mtn){
+		if (data->event == MOTION_IN) {
+			char motion = data->key.c_str()[0];
+			if (motion == mtn) {
+					data->event = KEY_OUT;
+					data->key = out;
+			 }
+	  }
+		return STATE_DEFAULT;
+	}
+	
+	state_t do_state_wait_ayg(instance_data_t *data){
+		return do_state_wait (data, 'g', 'y');
+	}
+	
+	state_t do_state_wait_ayq(instance_data_t *data){
+		return do_state_wait (data, 'q', 'y');
+	}
+	
+	state_t do_state_wait_ax(instance_data_t *data){
+	  return do_state_wait (data, 'z', 'x');
+	}
+
+	state_t do_state_wait_gz(instance_data_t *data){
+		return do_state_wait (data, 'j', 'z');		
+	}
+	
+}
---
+++ Projects/final_project/imu_fsm.h
@@ -0,0 +1,51 @@
+#ifndef IMU_FSM_H
+#define IMU_FSM_H
+
+#include "mbed.h"                    
+#include <string>
+
+namespace ImuFsm {
+	
+typedef enum {
+	STATE_DEFAULT,
+	STATE_WAIT_AY_Q,
+	STATE_WAIT_AY_G,
+	STATE_WAIT_AX,
+	STATE_WAIT_GZ,
+	NUM_STATES 
+} state_t;
+
+typedef enum {
+	GESTURE_IN,
+	MOTION_IN,
+	TIMEOUT_IN,
+	KEY_OUT
+} event_t;
+
+/* instance_data_t is used to pass input into fsm and return output to client.
+ * We do this by mutating the struct inside do_state_x functions. 
+ */ 
+typedef struct {
+	event_t event;
+	string key; // populated with gesture name if event=GESTURE,
+	            // populated with key pressed if event=RETURN_KEY
+		          // otherwise empty.
+}	instance_data_t;
+
+typedef state_t state_func_t(instance_data_t *data);
+state_t do_state_default(instance_data_t *data);
+state_t do_state_wait_ayq(instance_data_t *data);
+state_t do_state_wait_ayg(instance_data_t *data);
+state_t do_state_wait_ax(instance_data_t *data);
+state_t do_state_wait_gz(instance_data_t *data);
+
+state_func_t* const state_table[ NUM_STATES ] = {
+    do_state_default, do_state_wait_ayq, do_state_wait_ayg, do_state_wait_ax, do_state_wait_gz
+};
+
+state_t cur_state = STATE_DEFAULT;
+instance_data_t data;
+
+}
+
+#endif /* IMU_FSM_H */
