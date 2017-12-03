16acb1d6e4852e8578bd12eebc11a5d8f54e97bf
Ian Tay <skrpfn@lavabit.com>
2017-12-03 14:56:30 -0500

    cleanup of client side BT code
    Two modes of operation for client code:
    1: Test mode receives input and asks user what they actually meant to type. Stats are stored for future reference.
    2: Input mode converts character received through BT to a keypress.

--- Fakekeyboard/fake_kb.py
+++ Fakekeyboard/fake_kb.py
@@ -4,107 +4,107 @@ import time
 from subprocess import Popen, PIPE
 import threading
 
-class T1:
-    def __init__(self):
+
+class InputMode:
+    def __init__(self, port):
         self.item_available = False
         self.item = ""
         self.cv = threading.Condition()
-        # self.bt = serial.Serial(sys.argv[1], 9600)
+        self.bt = serial.Serial(port, 9600)
+
     def runWriter(self):
-        while(1):
+        while (1):
             self.cv.acquire()
             while not self.item_available:
                 self.cv.wait()
             self.item_available = False
             _ = [sendKey(x) for x in self.item]
             self.cv.release()
+
     def runReader(self):
         while (1):
-            c = input("")  
-            # c = self.bt.read().decode("utf-8") 
-            self.item=c
+            # c = input("")
+            c = self.bt.read().decode("utf-8")
+            self.item = c
             self.cv.acquire()
             self.item_available = True
             self.cv.notify()
             self.cv.release()
 
-class T2:
-    def __init__(self):
-        self.item_available = False
+
+class TestMode:
+    def __init__(self, port, username):
         self.item = ""
-        self.cv = threading.Condition()
         self.buffer = ""
-        # self.bt = serial.Serial(sys.argv[1], 9600)
+        self.bt = serial.Serial(port, 9600)
         self.wrong = 0
         self.total = 0
         self.done = False
-        self.test = ["test\n" for i in range(0,10)]
-    def runWriter(self):
-        while(1):
-            self.cv.acquire()
-            while not self.item_available:
-                self.cv.wait()
-            self.item_available = False
-            # sendKey(self.item) 
-            self.cv.release()
-            if self.done:
-                return
+        self.username = username
+        # self.test = ["test\n" for i in range(0,10)]
+
     def runReader(self):
         while (1):
-            c = self.test.pop()
-            # c = self.bt.read().decode("utf-8") 
-            self.item=c
+            print("read")
+            # c = self.test.pop()
+            c = self.bt.read().decode("utf-8")
+            self.item = c
             print(c)
-            self.cv.acquire()
             self.buffer += c
-            
+
             if "\n" in self.buffer:
                 actual = self.buffer.split("\n")[0]
                 ideal = input("expected word was: ")
                 self.buffer = ""
                 if actual != ideal:
                     self.wrong += 1
-                self.total+=1
+                self.total += 1
                 if self.total == 10:
-                    print("job's done, total = {0}, wrong = {1}".format(self.total, self.wrong))
+                    print(
+                        "job's done, total = {0}, wrong = {1}".format(
+                            self.total, self.wrong))
                     self.done = True
-            self.item_available = True
-            self.cv.notify()
-            self.cv.release()
             if self.done:
+                with open("results.txt", "w+") as f:
+                    f.write(
+                        "{0}: {1} wrong {2} total {3} % correct".format(
+                            self.username, self.wrong, self.total, float(self.total-self.wrong)/float(self.total)))
                 return
-            
+
+
 def sendKey(key):
     scpt = '''tell application \"System Events\" to keystroke \"''' + key + "\""
     scpt = scpt.encode('utf-8')
-    args=[]
+    args = []
     p = Popen(['osascript', '-'] + args, stdin=PIPE, stdout=PIPE, stderr=PIPE)
     stdout, stderr = p.communicate(scpt)
 
 
-def m():
-    if len(sys.argv) != 3:
-        print("not enough arguments. Usage: python3 fake_kb.py serialport mode\n modes: 1) INPUT 2) TEST")
+def run():
+    if len(sys.argv) < 3:
+        print("not enough arguments. Usage: python3 fake_kb.py serialport mode (testopts)"
+              + "\n modes: 1) INPUT 2) TEST"
+                + "\n testopts: username")
         return
     port = sys.argv[1]
     mode = sys.argv[2]
     if mode == "INPUT":
-        t = T1()
+        t = InputMode(port)
+        thread1 = threading.Thread(group=None, target=t.runReader, name="t1")
+        thread2 = threading.Thread(group=None, target=t.runWriter, name="t2")
+        thread1.start()
+        thread2.start()
     if mode == "TEST":
-        t = T2()
-    thread1 = threading.Thread(group=None,target=t.runReader,name="t1")
-    thread2 = threading.Thread(group=None,target=t.runWriter,name="t2")
-    thread1.start()
-    thread2.start()
+        if len(sys.argv) != 4:
+            print("not enough arguments. Usage: python3 fake_kb.py serialport mode (testopts)"
+                  + "\n modes: 1) INPUT 2) TEST"
+                    + "\n testopts: username")
+            return
+        username = sys.argv[3]
+        t = TestMode(port, username)
+        thread1 = threading.Thread(group=None, target=t.runReader, name="t1")
+        thread1.start()
+
 
 if __name__ == "__main__":
-    m()
-
-# if __name__ == "__main__":
-
-    # serialport = serial.Serial(sys.argv[1], 9600, timeout=5)
-    # while True:
-        # command = serialport.read().decode("utf-8") 
-        # sendKey(command)
-        # # sendKey(sys.argv[1])
-    # serialport.close()
+    run()
